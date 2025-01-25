969a26a8dde9e30c5dcd4ec2d4ed8428973ab38b
Pietro Mozzoni <79214368+Pietro-Mozzoni@users.noreply.github.com>
2025-01-25 13:10:45 +0100

    Fixata navbar superiore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
 function toggleMenu() {
     const menu = document.getElementById('dropdown-menu');
+    const button = document.querySelector('.menu-btn');
+
+    // Toggle menu visibility
     if (menu.style.display === 'block') {
         menu.style.display = 'none';
     } else {
         menu.style.display = 'block';
     }
+
+    // Toggle active state for the button
+    button.classList.toggle('active');
 }
-window.addEventListener('click', function(event) {
+
+window.addEventListener('click', function (event) {
     const menu = document.getElementById('dropdown-menu');
     const button = document.querySelector('.menu-btn');
+
     if (!menu.contains(event.target) && !button.contains(event.target)) {
         menu.style.display = 'none';
+        button.classList.remove('active');
     }
 });
