371c528d0049bbadd4e31a6f1c8bab44feab382a
Pietro Mozzoni <79214368+Pietro-Mozzoni@users.noreply.github.com>
2025-02-03 00:57:12 +0100

    altra versione

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,17 @@ window.addEventListener('click', function (event) {
         button.classList.remove('active');
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.3 });
+
+    elements.forEach(element => observer.observe(element));
+});
